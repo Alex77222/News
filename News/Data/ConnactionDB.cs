17f9cd6413150ea884ce01b0ee8b1aab5f5af6d7
“Alex <korolenkosasha644@gmail.com>
2022-08-23 17:05:37 +0300

    TEST!!!!!

---
+++ News/Data/ConnactionDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using News.Models;
+using System.Data;
+
+namespace News.Data
+{
+    public class ConnactionDB
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["adoConnectionstring"].ToString();
+
+        public List<User> GetUsers()
+        {
+            List<User> usersList = new List<User>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "select * from dbo.USERS with(nolock)";
+                SqlDataAdapter sqlData = new SqlDataAdapter(command);
+                DataTable drUsers = new DataTable();
+
+
+            }
+                return usersList;
+        }
+    }
+}
