ae247b2c30b0b8e3acb654449a10d6b608ed4619
Александр Короленко <korolenkosasha644@gmail.com>
2022-09-17 18:04:08 +0300

    Add wwwroot, and fixed ViewsNews

--- News/Startup.cs
+++ News/Startup.cs
@@ -22,6 +22,7 @@ namespace News
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
