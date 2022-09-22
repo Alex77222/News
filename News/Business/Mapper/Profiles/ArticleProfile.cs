bc90bfdcbb30ffd3c295e6ba155034956cd9d5f1
“Alex <korolenkosasha644@gmail.com>
2022-09-22 20:47:23 +0300

    Add mapper

---
+++ News/Business/Mapper/Profiles/ArticleProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using News.Models;
+
+namespace News.Business.Mapper.Profiles
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<Article,ArticleModel>()
+                .ForMember(
+                destinationMember dest:ArticleModel=>
+                )
+        }
+
+    }
+}
---
+++ News/Data/Entities/ArticleRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Data.Entities
+{
+    public class ArticleRepository
+    {
+        public List<Article> GetAllAricle()
+        {
+            return new List<Article>
+            {
+                new Article
+                {
+                    Id =1,
+                    Body = "Text in body lalalalal",
+                    Name="Article 1"
+                },
+                 new Article
+                {
+                    Id =2,
+                    Body = "Text in body 2 lalalgegegegegegalal",
+                    Name="Article 2"
+                },
+                   new Article
+                {
+                    Id =3,
+                    Body = "Text in body 3 lalalgegegpypypyyppegegegalal",
+                    Name="Article 3"
+                },
+
+
+            };
+        }
+    }
+}
