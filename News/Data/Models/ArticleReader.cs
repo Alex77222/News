ce66b5ae3222a8f3ae3c741bd336e227b0c9afcd
“Alex <korolenkosasha644@gmail.com>
2022-10-10 19:21:00 +0300

    Update BseRepossitory

---
+++ News/Data/Models/ArticleReader.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace News.Data.Models
+{
+    public class ArticleReader
+    {
+        public int ArticleId { get; set; }
+        public string ArticleHeader { get; set; }=string.Empty;
+        public string ArticleBody { get; set; } =string.Empty;
+        public int UserId { get; set; } 
+
+    }
+}
--- News/Data/Repositories/ArticleRepository.cs
+++ News/Data/Repositories/ArticleRepository.cs
@@ -1,80 +1,27 @@
-﻿using News.Data.Entities;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using News.Data.Entities;
 using News.Data.Interfaces;
-using News.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace News.Data.Repositories
 {
-    public class ArticleRepository : IArticleRepository
+    public class ArticleRepository : BaseRepository<Article>
     {
-        private List<Article> _articles;
-
-        public ArticleRepository()
+        public ArticleRepository(IConfiguration configuration):base(configuration)
+            { }
+        protected override string GetQueryForInsert(Article entity, string queryRaw)
         {
-            _articles = new List<Article>()
-            {
-                new Article
-                {
-                    Id =1,
-                    Body = "Text in body lalalalal",
-                    Name="Article 1"
-                },
-                 new Article
-                {
-                    Id =2,
-                    Body = "Text in body 2 lalalgegegegegegalal",
-                    Name="Article 2"
-                },
-                   new Article
-                {
-                    Id =3,
-                    Body = "Text in body 3 lalalgegegpypypyyppegegegalal",
-                    Name="Article 3"
-                },
-
-
-            };
-        }
-        public async Task<IList<Article>> GetListAsync()
-        {
-
-            return await Task.FromResult(_articles);
+            throw new System.NotImplementedException();
         }
 
-        public async Task<Article> GetListByIdAsync(int Id)
+        protected override string GetQueryForUpdate(Article entity, string queryRaw)
         {
-            return await Task.FromResult(_articles.FirstOrDefault(x => x.Id == Id));
+            throw new System.NotImplementedException();
         }
 
-        public async Task<IList<Article>> UpdateArticleAsync(ArticleViewModel model)
+        protected override IList<Article> ReadDataAsync(SqlDataReader reader)
         {
-            var ar = _articles.FirstOrDefault(x => x.Id == model.NewsId);
-            ar.Id = model.NewsId;
-            ar.Name = model.NewsHeader;
-            ar.Body = model.NewsText;
-            _articles.Remove(_articles.FirstOrDefault(x => x.Id == model.NewsId));
-            _articles.Add(ar);
-            return await Task.FromResult(_articles);
-        }
-        public async Task DeleteListByIdAsync(int Id)
-        {
-            var ar = _articles.FirstOrDefault(x => x.Id == Id);
-            _articles.Remove(ar);
-            
-        }
-        public async Task<IList<Article>> AddArticleAsync(ArticleViewModel model)
-        {
-
-            _articles.Add(new Article
-            {
-                Id = model.NewsId,
-               Body=model.NewsText,
-               Name=model.NewsHeader
-                
-            }); 
-            return await Task.FromResult(_articles);
+            throw new System.NotImplementedException();
         }
     }
-}
