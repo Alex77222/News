fb460375266c4f879e4b65006bb0bff3c040d88b
Александр Короленко <korolenkosasha644@gmail.com>
2022-10-27 18:57:03 +0300

    Add userrepository

---
+++ News/Data/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace News.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { set; } 
+    }
+}
---
+++ News/Data/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using News.Data.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace News.Data.Repositories
+{
+    public class UserRepository : BaseRepository<Article>
+    {
+        public UserRepository(IConfiguration configuration) : base(configuration)
+        { }
+        protected override string GetQueryForInsert(Article entity, string queryRaw)
+        {
+            return string.Format(queryRaw, entity.Header, entity.Body);
+        }
+
+        protected override string GetQueryForUpdate(Article entity, string queryRaw)
+        {
+            return string.Format(queryRaw, entity.Header, entity.Body, entity.Id);
+        }
+
+        protected override IList<Article> ReadDataAsync(SqlDataReader reader)
+        {
+            var artilce = new List<Article>();
+            while (reader.Read())
+            {
+                artilce.Add(new Article
+                {
+                    Id = Convert.ToInt32((reader["Id"].ToString() ?? string.Empty)),
+                    Header = reader["Header"].ToString() ?? string.Empty,
+                    Body = reader["Body"].ToString() ?? string.Empty,
+                });
+
+            }
+            return artilce;
+        }
+    }
+}
