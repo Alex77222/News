15634de814298a4c3303593e7270c0ed437d6b5e
“Alex <korolenkosasha644@gmail.com>
2022-11-06 18:11:20 +0300

    Add RoleService

---
+++ News/Business/Services/Interfaces/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace News.Business.Services.Interfaces
+{
+    public interface IRoleService
+    {
+        public Task DeleteRoleAsync(string userName);
+        public Task AddRoleByUserNameAsync(string userName);
+        public Task<IList<string>> GetLidtRoleAsync();
+        public Task<IList<string>> GetRolesAsync();
+        public Task<IList<string>> GetRolesByUserNameAsync(string userName);
+    }
+}
---
+++ News/Business/Services/RoleService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using News.Business.Services.Interfaces;
+using News.Data;
+using News.Data.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace News.Business.Services
+{
+    public class RoleService :IRoleService
+    {
+        private readonly UnitOfWork _db;
+        private readonly IConfiguration _configuration;
+        public RoleService(IConfiguration configuration, UnitOfWork unitOfWork)
+        {
+            _configuration = configuration;
+            _db = unitOfWork;
+        }
+
+        public async Task AddRoleByUserNameAsync(string userName)
+        {
+            var userId = (await _db.Users.GetSingleAsync(userName)).Id;
+            var role = await _db.Roles.GetSingleAsync("user");
+
+            if (role == null)
+            {
+                return;
+            }
+            await _db.UserRoles.AddAsync(new UserRoles
+            {
+                UserId = userId,
+                RoleId = role.Id,
+            });
+        }
+
+        public Task DeleteRoleAsync(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<string>> GetLidtRoleAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesByUserNameAsync(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
